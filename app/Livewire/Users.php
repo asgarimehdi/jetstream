7f3ab1b7d2f29cf56df9a73388edc622da2fabe2
mehdi asgari <it1@abharh.ir>
2023-11-11 14:16:03 +0330

    data table

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,13 +3,45 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Users extends Component
 {
+    use WithPagination;
+    #[Url(history:true)]
+    public $search='';
+    #[Url(history:true)]
+    public $admin='';
+    #[Url(history:true)]
+    public $sortBy='created_at';
+    #[Url(history:true)]
+    public $sortDir='DESC';
+    #[Url()]
+    public $perPage = 5;
+    public function updatedSearch(){
+        $this->resetPage();
+    }
+    public function delete(User $user){
+        $user->delete();
+    }
+    public function setSortBy($sortByField){
+        if($this->sortBy===$sortByField){ // agar haman field sort shode bod hala bar ax sort kon
+            $this->sortDir=($this->sortDir=="ASC") ? 'DESC' : 'ASC';
+            return;
+        }
+        $this->sortBy=$sortByField;
+        $this->sortDir='DESC'; // pishfarz desc sort kon
+    }
     public function render()
     {
-        $users=User::all();
+        $users=User::search($this->search)
+            ->when($this->admin !=='',function ($query){
+                $query->where('is_admin',$this->admin);
+            })
+            ->orderBy($this->sortBy,$this->sortDir)
+            ->paginate($this->perPage);
         return view('livewire.users',['users'=>$users]);
     }
 }
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,71 +1,115 @@
-<div dir="rtl">
+<div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight text-right">
             مدیریت کاربران
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div
-                    class="p-6 lg:p-8  border-b border-gray-200 bg-gradient-to-r from-rose-200 via-fuchsia-200 to-white">
-
-
-                    <h1 class="mt-2 text-2xl font-medium text-gray-900">
-                        Welcome to User manager
-                    </h1>
-
-                    <p class="mt-6 text-gray-500 leading-relaxed">
-                        this is user manager table
-                    </p>
-
-                    @foreach($users as $user)
-
-                        {{$user->id}} -
-                        {{$user->name}} <br/>
-                    @endforeach
-                    <div x-data="{ open: false }">
-                        <button x-on:click="open = ! open">Toggle Dropdown</button>
-                        <div x-show="open">
-                            <x-modal>
-                                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                    <div class="sm:flex sm:items-start">
-                                        <div
-                                            class="mx-auto shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                                            <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg"
-                                                 fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                 stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"/>
-                                            </svg>
-                                        </div>
-
-                                        <div class="mt-3 text-center sm:mt-0 sm:ms-4 sm:text-start">
-                                            <h3 class="text-lg font-medium text-gray-900">
-                                                {{--                                        {{ $title }}--}}
-                                                Hello
-                                            </h3>
-
-                                            <div class="mt-4 text-sm text-gray-600">
-                                                {{--                                        {{ $content }}--}}
-                                                Contentaam
-                                            </div>
-
-                                        </div>
-                                    </div>
+    <div>
+        <section class="mt-10">
+            <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
+                <!-- Start coding here -->
+                <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
+                    <div class="flex items-center justify-between d p-4">
+                        <div class="flex">
+                            <div class="relative w-full">
+                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                         fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd"
+                                              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                              clip-rule="evenodd"/>
+                                    </svg>
                                 </div>
-
-                                <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-end">
-                                    {{--                            {{ $footer }}--}}
-
-                                    Footer hastam
-                                </div>
-                            </x-modal>
+                                <input wire:model.live.debounce.300ms="search" type="text"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 "
+                                       placeholder="Search" required="">
+                            </div>
                         </div>
+                        <div class="flex space-x-3">
+                            <div class="flex space-x-3 items-center">
+                                <label class="w-40 text-sm font-medium text-gray-900">User Type :</label>
+                                <select
+                                    wire:model.live="admin"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                    <option value="">All</option>
+                                    <option value="0">User</option>
+                                    <option value="1">Admin</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="overflow-x-auto">
+                        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                            <tr>
+                                @include('livewire.includes.table-sortable-th',[
+                                    'name'=>'name',
+                                    'displayName'=>'Name'
+                                ])
+                                @include('livewire.includes.table-sortable-th',[
+                                    'name'=>'email',
+                                    'displayName'=>'Email'
+                                ])
+                                @include('livewire.includes.table-sortable-th',[
+                                    'name'=>'is_admin',
+                                    'displayName'=>'Role'
+                                ])
+                                @include('livewire.includes.table-sortable-th',[
+                                    'name'=>'created_at',
+                                    'displayName'=>'Joined'
+                                ])
+                                @include('livewire.includes.table-sortable-th',[
+                                    'name'=>'updated_at',
+                                    'displayName'=>'Last update'
+                                ])
+                                <th scope="col" class="px-4 py-3">
+                                    <span class="sr-only">Actions</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
+                                <tr wire:key="{{$user->id}}" class="border-b dark:border-gray-700">
+                                    <th scope="row"
+                                        class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{$user->name}}</th>
+                                    <td class="px-4 py-3">{{$user->email}}</td>
+                                    <td class="px-4 py-3 {{$user->is_admin ? 'text-green-500' : 'text-blue-500'}} ">
+                                        {{$user->is_admin ? 'Admin' : 'Member'}}</td>
+                                    <td class="px-4 py-3">{{$user->created_at}}</td>
+                                    <td class="px-4 py-3">{{$user->updated_at}}</td>
+                                    <td class="px-4 py-3 flex items-center justify-end">
+                                        <button onClick="confirm('Are you sure?')" wire:click="delete({{$user->id}})"
+                                                class="px-3 py-1 bg-red-500 text-white rounded">X
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="py-4 px-3">
+                        <div class="flex ">
+                            <div class="flex space-x-4 items-center mb-3">
+                                <label class="w-32 text-sm font-medium text-gray-900">Per Page</label>
+                                <select
+                                    wire:model.live="perPage"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="20">20</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </div>
+                        </div>
+                        {{$users->links()}}
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
     </div>
 </div>
