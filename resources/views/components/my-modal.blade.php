6bc6be88bceb6605c1b38eb93b61af1ed64f2566
mehdi asgari <it1@abharh.ir>
2023-11-14 14:10:47 +0330

    working on myModal

---
+++ resources/views/components/my-modal.blade.php
@@ -0,0 +1,25 @@
+@props(['name','title'])
+<div
+    x-data="{show : false,name:'{{$name}}'}"
+    x-show="show"
+    x-on:open-modal.window="show=($event.detail.name===name)"
+    x-on:close-modal.window="show=false"
+    x-on:keydown.escape.window="show=false"
+    style="display: none"
+    x-transition
+    class="fixed z-50 inset-0">
+    {{--    Gray Background--}}
+    <div x-on:click="show=false" class="fixed inset-0 bg-gray-800 opacity-60"></div>
+    {{--    Modal Body--}}
+    <div class="bg-white rounded m-auto fixed inset-0 max-w-2xl h-40">
+
+        <div><button  x-on:click="$dispatch('close-modal')">X</button></div>
+        @if(isset($title))
+        <div class="py-3 flex items-center justify-center">
+            {{$title}}
+        </div>
+        @endif
+        <div>{{$body}}</div>
+
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,6 +8,23 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                <x-my-modal name="test" title="Modal 2">
+                    <x-slot:body>
+                        testeredfd fdhjfhd f
+                    </x-slot>
+                </x-my-modal>
+                <x-my-modal name="test3" title="Modal 3">
+                    <x-slot:body>
+                        fdfdfdfdfdfdfdf
+                    </x-slot>
+                </x-my-modal>
+                <button x-data x-on:click="$dispatch('open-modal',{name:'test'})" class="px-3 py-1 bg-teal-500 text-white rounded">
+                    Open Modal
+                </button>
+                <button x-data x-on:click="$dispatch('open-modal',{name:'test3'})" class="px-3 py-1 bg-teal-500 text-white rounded">
+                    Open Modal3
+                </button>
                 <x-welcome />
             </div>
         </div>
